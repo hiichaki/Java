ce734879daf06859284cfc20f6ba5c961b6e1ae6
hiichaki <hiichaki350@gmail.com>
2016-02-26 05:11:04 +0200

    done change

--- pr12/src/pr12/Question.java
+++ pr12/src/pr12/Question.java
@@ -10,12 +10,63 @@ package pr12;
  * @author hiichaki
  */
 public class Question {
-    String q,a,b,c,d;
+    private String q;
+    private String a;
+    private String b;
+    private String c;
+    boolean A, B, C;
     
-    void setQuestion (String _q, String _a, String _b, String _c){
+    public void setQuestion (String _q, boolean aa, String _a, boolean bb, String _b, boolean cc, String _c){
         q=_q;
-        a=_a;
-        b=_b;
-        c=_c;
+        a=_a; A=aa;
+        b=_b; B=bb;
+        c=_c; C=cc;
     }
+
+    
+    public String get(int n){
+        String tmp = new String();
+        switch(n){
+            case 0: tmp= a; break;
+            case 1: tmp = b; break;
+            case 2: tmp = c; break;
+        }
+        return tmp;
+    }
+    
+    public int rigth(int n){
+        return 0;
+        
+    
+    }
+    
+    /**
+     * @return the a
+     */
+    public String getA() {
+        return a;
+    }
+
+    /**
+     * @return the b
+     */
+    public String getB() {
+        return b;
+    }
+
+    /**
+     * @return the c
+     */
+    public String getC() {
+        return c;
+    }
+
+    /**
+     * @return the q
+     */
+    public String getQ() {
+        return q;
+    }
+
+    
 }
--- pr12/src/pr12/Test.java
+++ pr12/src/pr12/Test.java
@@ -5,12 +5,20 @@
  */
 package pr12;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.AbstractButton;
+import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
@@ -19,32 +27,82 @@ import javax.swing.JRadioButton;
  * @author hiichaki
  */
 public class Test extends JFrame {
-    Question[] qs = new Question[5];
-    
+    Question[] qs = new Question[4];
+    static int n=0;
+    boolean f=true;
+    JPanel tmp = new JPanel();
     Test(){
         super("Test");
+        
+        for(int i=0;i<qs.length;++i)
+            qs[i] = new Question();
+        
+        qs[0].setQuestion("WTF?", false, "azaza", false, "trololo", true, "qwe");
+        qs[1].setQuestion("azaza?", false, "lol", true, "qwerty", false, "asdfg");
+        qs[2].setQuestion("bugaga?", true, "wow", false, "zxcvb", false, "asdfg");
+        qs[3].setQuestion("???", false, "dfg", false, "cvb", true, "qazwsx");
+        
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel panel = new JPanel(new GridLayout(0, 1));
+       
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        int w,h;
+        w=(int) screen.getWidth();
+        h=(int) screen.getHeight();
+        this.setBounds(w/3, h/3, 350, 250);
 
-        ButtonGroup group = new ButtonGroup();
-        JRadioButton aRadioButton = new JRadioButton("A");
-        JRadioButton bRadioButton = new JRadioButton("B");
+        Container c = getContentPane();
+        tmp = next();
+        c.add(tmp,BorderLayout.NORTH);
+        
+        JButton n = new JButton("next");
+        JPanel nn = new JPanel();
+        nn.add(n, BorderLayout.SOUTH);
+        nn.setBorder(BorderFactory.createEtchedBorder());
+        c.add(nn);
+        
+        n.addActionListener((ActionEvent e) -> {
+            c.remove(tmp);
+            tmp=next();
+            c.add(tmp,BorderLayout.NORTH);
+            revalidate();
+            repaint();
+        });
+        
+        
+        setVisible(true);
+    }
+    
+    JPanel next(){
+        if(n>3)
+            return new JPanel();
+        
+        JPanel questionPanel = new JPanel(new GridLayout(4,1));
+        questionPanel.setBorder(BorderFactory.createEtchedBorder());
+        JPanel pane = new JPanel(new FlowLayout());
 
         ActionListener ac = (ActionEvent actionEvent) -> {
             AbstractButton aButton = (AbstractButton) actionEvent.getSource();
             System.out.println("Selected: " + aButton.getText());
         };
-            
-        aRadioButton.addActionListener(ac);
-        bRadioButton.addActionListener(ac);
 
-        panel.add(aRadioButton);
-        group.add(aRadioButton);
-        panel.add(bRadioButton);
-        group.add(bRadioButton);
+        ButtonGroup group = new ButtonGroup();
+        JRadioButton[] RB = new JRadioButton[3];     
 
-        add(panel);
-        setSize(300, 200);
-        setVisible(true);
+        for(int j=0;j<3;++j){
+            RB[j] =new JRadioButton(qs[n].get(j));
+            group.add(RB[j]);
+        }
+
+        pane.add(new JLabel(qs[n].getQ()));
+        questionPanel.add(pane);
+
+        for(int j=0;j<3;++j){
+            pane = new JPanel();
+            pane.add(RB[j]);
+            questionPanel.add(pane);
+            RB[j].addActionListener(ac);
+        }
+        n++;
+        return questionPanel;
     }
 }
